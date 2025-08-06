97dd0c33b13a8f05a48c8e52eee092394c0aab8c
Pascalblaq <pascalblaq1414@gmail.com>
2025-08-06 23:54:54 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~QusCgRJQ#3bk799xrCeMp8_-lJ6CdKaeupgxE3gISHoKNnOQA9MU",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348124211232", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
